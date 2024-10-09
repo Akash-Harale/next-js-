020c49a495da1bf61db03567dbb7ee483e251daf
Akash Harale <akash.v.harale@gmail.com>
2024-10-09 14:15:21 +0530

    completed routing

---
+++ src/app/products/[productId]/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ProductDetail = ({ params }: {
+    params: {productId: string}
+}) => {
+  return (
+    <h1
+    
+    >ProductDetail {params.productId}</h1
+    
+    >
+  )
+}
+
+export default ProductDetail
